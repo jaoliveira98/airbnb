6980495205c1cf51108240049651bd4c7556b877
Joao Oliveira <jaoliveira98@gmail.com>
2023-05-09 11:47:09 +0100

    RegisterModal imports

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import axios from "axios";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
 const RegisterModal = () => {
-  return <div>RegisterModal</div>;
+  return <div></div>;
 };
 
 export default RegisterModal;
