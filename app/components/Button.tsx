07aca0f6c1b857dae4f22a800fdab91c7093c25d
Joao Oliveira <jaoliveira98@gmail.com>
2023-05-09 14:44:24 +0100

    Register Component UI

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -27,11 +27,12 @@ const Button: React.FC<ButtonProps> = ({
       ${
         outline ? "bg-white border-black text-black" : "bg-rose-500 text-white"
       } 
+      
       ${
         small
           ? "text-sm py-1 font-light border-[1px]"
           : "text-md py-3 font-semibold border-2"
-      }`}
+      } `}
     >
       {Icon && <Icon size={24} className="absolute left-4 top-3" />}
       {label}
