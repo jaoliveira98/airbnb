d922fe3e297cd4a706a4d6a083963a23de0ce79d
Joao Oliveira <jaoliveira98@gmail.com>
2023-05-09 13:32:04 +0100

    Register User WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-rose-500 text-2xl flex items-center justify-center h-full">
-      Loading...
+      Welcome
     </div>
   );
 }
